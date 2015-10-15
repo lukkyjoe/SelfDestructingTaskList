f420df023a182d58f9577d61509eefb5553871bf
Joseph Luk <joseph.sc.luk@gmail.com>
2015-10-15 23:43:11 +0000

    for john to look at

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,36 @@
 var app = angular.module("myTaskList", ["ui.router", "firebase"]);
+
+app.controller("landingController", function($scope, $firebaseObject) {
+  var ref = new Firebase("https://popping-torch-4959.firebaseio.com/data");
+  // download the data into a local object
+  var syncObject = $firebaseObject(ref);
+  // synchronize the object with a three-way data binding
+  // click on `index.html` above to see it used in the DOM!
+  syncObject.$bindTo($scope, "data").then(function(){console.log($scope.data)},function(error){console.log(error)})
+});
+
+app.config(['$stateProvider','$locationProvider', function($stateProvider, $locationProvider){
+   $locationProvider.html5Mode(true);
+ 
+   $stateProvider.state('landing', {
+     url: '/',
+     controller: 'landingController',
+     template: "<input type='text' ng-model='data.text'/>"
+    })
+  }]);
+              
+//app.controller('landingController',['$scope',function($scope){}]);
+
+// blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+//    $locationProvider.html5Mode(true);
+ 
+//    $stateProvider.state('landing', {
+//      url: '/',
+//      controller: 'Landing.controller',
+//      templateUrl: '/templates/landing.html'
+//    });
+//  }]);
+ 
+//  // This is a cleaner way to call the controller than crowding it on the module definition.
+//  blocJams.controller('Landing.controller', ['$scope', function($scope) {
+//    $scope.subText = "Turn the music up!";
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,36 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var app = angular.module("myTaskList", ["ui.router", "firebase"]);
 
-},{}]},{},[1]);
+app.controller("landingController", function($scope, $firebaseObject) {
+  var ref = new Firebase("https://popping-torch-4959.firebaseio.com/data");
+  // download the data into a local object
+  var syncObject = $firebaseObject(ref);
+  // synchronize the object with a three-way data binding
+  // click on `index.html` above to see it used in the DOM!
+  syncObject.$bindTo($scope, "data").then(function(){console.log($scope.data)},function(error){console.log(error)})
+});
+
+app.config(['$stateProvider','$locationProvider', function($stateProvider, $locationProvider){
+   $locationProvider.html5Mode(true);
+ 
+   $stateProvider.state('landing', {
+     url: '/',
+     controller: 'landingController',
+     template: "<input type='text' ng-model='data.text'/>"
+    })
+  }]);
+              
+//app.controller('landingController',['$scope',function($scope){}]);
+
+// blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+//    $locationProvider.html5Mode(true);
+ 
+//    $stateProvider.state('landing', {
+//      url: '/',
+//      controller: 'Landing.controller',
+//      templateUrl: '/templates/landing.html'
+//    });
+//  }]);
+ 
+//  // This is a cleaner way to call the controller than crowding it on the module definition.
+//  blocJams.controller('Landing.controller', ['$scope', function($scope) {
+//    $scope.subText = "Turn the music up!";
