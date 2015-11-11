489abca4005a1ee530cb3804975c10e594fc8ff0
Joseph Luk <joseph.sc.luk@gmail.com>
2015-11-11 02:29:21 +0000

    firebase array show

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,23 +1,31 @@
 var app = angular.module("myTaskList", ["ui.router", "firebase"]);
 
-app.controller("landingController", function($scope, $firebaseObject) {
-  var ref = new Firebase("https://popping-torch-4959.firebaseio.com/data");
+app.controller("landingController", function($scope, $firebase, $firebaseObject, $firebaseArray) {
+  var ref = new Firebase("https://popping-torch-4959.firebaseio.com/messages");
+  $scope.messages = $firebaseArray(ref);
+  
+  // add new items to the array
+  // the message is automatically added to our Firebase database!
+//   $scope.addMessage = function() {
+//     $scope.messages.$add($scope.data.text);
+//   };
+  
   // download the data into a local object
   var syncObject = $firebaseObject(ref);
   // synchronize the object with a three-way data binding
   // click on `index.html` above to see it used in the DOM!
-  syncObject.$bindTo($scope, "data").then(function(){console.log($scope.data)},function(error){console.log(error)})
+  syncObject.$bindTo($scope, "data")
 });
 
-app.config(['$stateProvider','$locationProvider', function($stateProvider, $locationProvider){
-   $locationProvider.html5Mode(true);
+// app.config(['$stateProvider','$locationProvider', function($stateProvider, $locationProvider){
+//    $locationProvider.html5Mode(true);
  
-   $stateProvider.state('landing', {
-     url: '/',
-     controller: 'landingController',
-     template: "<input type='text' ng-model='data.text'/>"
-    })
-  }]);
+//    $stateProvider.state('landing', {
+//      url: '/',
+//      controller: 'landingController',
+//      template: "<input type='text' ng-model='data.text'/>"
+//     })
+//   }]);
               
 //app.controller('landingController',['$scope',function($scope){}]);
 
