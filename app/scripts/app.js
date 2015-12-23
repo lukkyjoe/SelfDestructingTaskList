146835f6914316e0296ed3d27b49889a00ce8f3d
Joseph Luk <joseph.sc.luk@gmail.com>
2015-12-23 00:14:50 +0000

    how to mark complete?

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,16 +11,17 @@ app.config(['$stateProvider','$locationProvider', function($stateProvider, $loca
    $stateProvider.state('landing', {
      url: '/',
      controller: 'landingController',
-     template: "<ul ng-repeat='item in messages | orderBy:\"priority\"'> <li ng-show='timeDiff(item) < 30'>{{item.text}} : {{item.priority}} : {{item.timestamp}}</li> </ul>"
+     template: "<ul ng-repeat='item in messages | orderBy:\"priority\"'> <li ng-show='timeDiff(item) < 30'>{{item.text}} : {{item.priority}} : {{item.timestamp}} : {{item.status}} : <a ng-click="markCompleted()"><input type=submit value=complete></a> </li>  </ul>"
 //      <input type=text ng-model='data.expirationdate'>
     })
    $stateProvider.state('landing2', {
      url: '/expired',
      controller: 'landingController',
-     template: "testing landing2 <ul ng-repeat='item in messages'> <li ng-show='timeDiff(item) >= 30'>{{item.text}} : {{item.priority}} : {{item.timestamp}}</li> </ul>"
+     template: "testing landing2 <ul ng-repeat='item in messages'> <li ng-show='timeDiff(item) >= 30'>{{item.text}} : {{item.priority}} : {{item.timestamp}} : {{item.status}}</li> </ul>"
     })   
   }]);
 
+//here we inject a bunch of services that help us do cool stuff
 app.controller("landingController", function($scope, $firebase, $firebaseObject, $firebaseArray) {
   var ref = new Firebase("https://popping-torch-4959.firebaseio.com/messages");
   $scope.messages = $firebaseArray(ref);
@@ -31,19 +32,23 @@ app.controller("landingController", function($scope, $firebase, $firebaseObject,
       text: $scope.newMessageText,
       timestamp: Math.floor(Date.now()/1000),
       priority: $scope.priority,
-      completed: false
+      status: "active"
     });
   
   };
   
-//   $scope.updateMessage = function(){
-//     $scope.messages.$update
-//   }
+  //shows seconds past since the epoch
   $scope.timeDiff = function(item) {
     return (Date.now()/1000) - item.timestamp
   }
 
-   
+  //update a task to complete 
+ $scope.markComplete = function(){
+   $scope.messages.$save({
+     status: "complete"
+   }
+   )
+ }
 });
 
 
